f4e83f4c8111f946041cd6cd09847dab022c0fe2
a.marucci <a.marucci@ifarhu.gob.pa>
2016-08-01 15:39:08 -0500

    IB - Agregando favicon, ajuste de estilos del logo de DTI, cambiando a espacios a tabs, agregando sentencias gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,36 @@
 var gulp = require('gulp'), 
-    sass = require('gulp-ruby-sass') ,
-    notify = require("gulp-notify") ,
-    minify = require('gulp-minify-css');
-    uglify  = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    convert = require('gulp-sass'),
-    bower = require('gulp-bower');
-    
+	sass = require('gulp-ruby-sass') ,
+	notify = require("gulp-notify") ,
+	minify = require('gulp-minify-css');
+	uglify  = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	convert = require('gulp-sass'),
+	bower = require('gulp-bower');
+	
 var config = {
-     sassPath: './assets/css/',
-    siteCSS: '._site/assets/css/'
+	sassPath: './assets/css',
+	siteCSS: './_site/build/css',
+
 };
 
-gulp.task('bower', function () { 
-    return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
-});
-
 gulp.task('css', function() { 
-    return gulp.src(config.sassPath + 'main.scss')
-         .pipe(sass({
-             style: 'compressed',
-             loadPath: [
-                 config.sassPath
-             ]
-         }) 
-        .on("error", notify.onError(function (error) {
-            return "Error: " + error.message;
-        }))) 
-         .pipe(gulp.dest('.')); 
+	return gulp.src(config.sassPath + '/main.scss')
+		.pipe(sass({
+			style: 'compressed',
+			loadPath: [
+				config.sassPath,
+			]
+		}) 
+		.on("error", notify.onError(function (error) {
+			return "Error: " + error.message;
+		}))) 
+		.pipe(gulp.dest(config.siteCSS)); 
 });
 
 // Rerun the task when a file changes
  gulp.task('watch', function () {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+	gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
 });
 
   gulp.task('default', ['css']);
